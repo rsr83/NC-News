1a108f3537cd8cb30a4235fdd104db71fd57541b
rsr83 <rafael.ramalho@gmail.com>
2024-02-06 14:38:01 +0000

    this post add comments for an article - task 6

--- src/ArticleCard.jsx
+++ src/ArticleCard.jsx
@@ -1,24 +1,14 @@
 import { Link } from "react-router-dom";
 
 export default function ArticleCard({ article }) {
-  const {
-    article_id,
-    article_img_url,
-    author,
-    comment_count,
-    created_at,
-    title,
-    topic,
-    votes,
-  } = article;
+  const { article_id, article_img_url, title } = article;
 
   return (
     <>
       <div className="article-card">
-        <img src={article_img_url}></img>
-        <h2>{title}</h2>
         <Link to={`/articles/${article_id}`}>
-          See Article Details and Comments
+          <img src={article_img_url}></img>
+          <h2>{title}</h2>
         </Link>
         ;
       </div>
