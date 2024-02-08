3656b22324efac2d0aba01b64ef9ac8bfe17fcfb
rsr83 <rafael.ramalho@gmail.com>
2024-02-08 15:18:33 +0000

    this commit adds filter by topic option task 10

--- src/Articles.jsx
+++ src/Articles.jsx
@@ -1,9 +1,12 @@
 import { useState, useEffect } from "react";
 import ArticleCard from "./ArticleCard";
 import fetchArticles from "./Utils/fetchArticles";
+import fetchTopics from "./Utils/fetchTopics";
+import { Link } from "react-router-dom";
 
 export default function Articles() {
   const [articlesList, setArticlesList] = useState([]);
+  const [topicsList, setTopicsList] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
@@ -14,8 +17,24 @@ export default function Articles() {
     });
   }, []);
 
+  useEffect(() => {
+    fetchTopics().then(({ topics }) => {
+      setTopicsList(topics);
+    });
+  }, []);
+
   return (
     <>
+      <div className="topics">
+        <h3>filter articles by topic:</h3>
+        {topicsList.map((topic) => {
+          return (
+            <Link key={topic.slug} to={`/articles/topic/${topic.slug}`}>
+              <p>{topic.slug}</p>
+            </Link>
+          );
+        })}
+      </div>
       {isLoading ? (
         <p>Loading...</p>
       ) : (
