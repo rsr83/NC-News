763cfd4acb96c49fe1d243b8ff34b8badb641df8
rsr83 <rafael.ramalho@gmail.com>
2024-02-08 12:07:54 +0000

    this commit gives the user an  option to delete their own comments task 9

--- src/CommentCard.jsx
+++ src/CommentCard.jsx
@@ -1,11 +1,45 @@
-export default function CommentCard({ comment }) {
-  const { body, author, created_at } = comment;
+import { useState, useEffect } from "react";
+import deleteCommentByID from "./Utils/deleteCommentByID";
 
-  return (
-    <div className="comment-card">
-      <p>{body}</p>
-      <p>comment by: {author}</p>
-      <p>created at: {created_at.slice(0, 10)}</p>
-    </div>
-  );
+export default function CommentCard({ comment, commentList, setCommentList }) {
+  const [deleteComment, setDeleteComment] = useState(false);
+  const [hasDeleted, setHasDeleted] = useState(false);
+  const { body, author, created_at, comment_id } = comment;
+  const condition = author === "grumpy19";
+
+  useEffect(() => {
+    if (deleteComment === true) {
+      deleteCommentByID(comment_id).then(() => {
+        setDeleteComment(false);
+      });
+    }
+  }, [deleteComment]);
+
+  function handleClickDelete() {
+    setDeleteComment(true);
+    setHasDeleted(true);
+  }
+
+  if (hasDeleted === false) {
+    return (
+      <div className="comment-card">
+        <p>{body}</p>
+        <p>comment by: {author}</p>
+        <p>created at: {created_at.slice(0, 10)}</p>
+        {condition ? (
+          <button
+            onClick={() => {
+              handleClickDelete();
+            }}
+          >
+            delete
+          </button>
+        ) : (
+          <></>
+        )}
+      </div>
+    );
+  } else {
+    return <p>comment deleted</p>;
+  }
 }
